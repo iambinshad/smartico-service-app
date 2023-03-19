f3f8e0aa428ea3498c0a93f6f96621c359d62e09
iambinshad <binshadp999@gmail.com>
2023-03-19 20:19:09 +0530

    endpoints and base url add

---
+++ lib/core/api/api_configration.dart
@@ -0,0 +1,13 @@
+class ApiConfigtration{
+  static const kIpAddress = '10.4.4.131';
+  static const kBaseUrl = "http://10.0.13.81:3500";
+
+  //userEndPoints
+
+  static const login = '/login';
+  static const otp = '/OTP';
+  static const verifyOtp = '/verifyOTP';
+ 
+  
+  
+}
