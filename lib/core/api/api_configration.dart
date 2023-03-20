068b09428e48307d4cf7c575b706734f1be625e7
iambinshad <binshadp999@gmail.com>
2023-03-20 11:27:11 +0530

    ip address edit

--- lib/core/api/api_configration.dart
+++ lib/core/api/api_configration.dart
@@ -1,6 +1,6 @@
 class ApiConfigtration{
   static const kIpAddress = '10.4.4.131';
-  static const kBaseUrl = "http://10.0.13.81:3500";
+  static const kBaseUrl = "http://$kIpAddress:3500";
 
   //userEndPoints
 
