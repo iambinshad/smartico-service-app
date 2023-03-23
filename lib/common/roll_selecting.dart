7e0e000cef7edeae51be9902a4ea104e80ce5d60
iambinshad <binshadp999@gmail.com>
2023-03-23 15:13:06 +0530

    Ui updated

--- lib/common/roll_selecting.dart
+++ lib/common/roll_selecting.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-import '../../core/widgets.dart';
-import '../vendor/vendor_sign_in.dart';
+import '../core/widgets.dart';
+import '../vendor/view/authentication/vendor_sign_in.dart';
 
 class RollSelectingScreen extends StatelessWidget {
   RollSelectingScreen({super.key});
@@ -33,85 +33,65 @@ class RollSelectingScreen extends StatelessWidget {
                     padding: const EdgeInsets.all(5.0),
                     child: Card(
                       elevation: 4.0,
-                      child: Column(children: [
-                        kHeight40,
-                        kHeight10,
-                        const Text(
-                          'Looking For A Specialist',
-                          style: TextStyle(
-                              fontSize: 35,
-                              color: Color.fromARGB(255, 123, 230, 219),
-                              fontFamily: 'Roboto',
-                              fontWeight: FontWeight.bold),
-                        ),
-                        kHeight40,
-                        const Center(
-                          child: Text(
-                            'To place any type of order to',
+                      child: Column(
+                        children: [
+                          kHeight40,
+                          kHeight10,
+                          const Text(
+                            'Looking For A Worker',
+                            style: TextStyle(
+                                fontSize: 35,
+                                color: Color.fromARGB(255, 123, 230, 219),
+                                fontFamily: 'Roboto',
+                                fontWeight: FontWeight.bold),
+                          ),
+                          kHeight40,
+                          const Center(
+                            child: Text(
+                              'To place any type of order',
+                              style: TextStyle(
+                                  fontSize: 25, fontWeight: FontWeight.w500),
+                            ),
+                          ),
+                          const Text(
+                            'to search for a performer',
                             style: TextStyle(
                                 fontSize: 25, fontWeight: FontWeight.w500),
                           ),
-                        ),
-                        const Text(
-                          'search for a performer',
-                          style: TextStyle(
-                              fontSize: 25, fontWeight: FontWeight.w500),
-                        ),
-                        kHeight40,
-                        ElevatedButton(
-                          onPressed: () {
-                            // Navigator.push(context, MaterialPageRoute(builder: (context) => const RollSelectingScreen(),));
-                          },
-                          style: ButtonStyle(
-                            elevation: MaterialStateProperty.all(5.0),
-                            fixedSize: MaterialStateProperty.all(
-                              const Size(280, 45),
-                            ),
-                            backgroundColor: MaterialStateProperty.all(
-                              const Color.fromARGB(255, 123, 230, 219),
-                            ),
-                            shape: MaterialStateProperty.all(
-                              RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10),
+                          kHeight40,
+                          ElevatedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => VendorSignIn(),
+                                  ));
+                            },
+                            style: ButtonStyle(
+                              elevation: MaterialStateProperty.all(5.0),
+                              fixedSize: MaterialStateProperty.all(
+                                const Size(280, 45),
+                              ),
+                              backgroundColor: MaterialStateProperty.all(
+                                const Color.fromARGB(255, 123, 230, 219),
+                              ),
+                              shape: MaterialStateProperty.all(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
                               ),
                             ),
+                            child: const Text(
+                              'I Need Service',
+                              style:
+                                  TextStyle(color: Colors.black, fontSize: 25),
+                            ),
                           ),
-                          child: const Text(
-                            'I Need Service',
-                            style: TextStyle(color: Colors.black, fontSize: 25),
-                          ),
-                        ),
-                      ]),
+                        ],
+                      ),
                     ),
                   ),
                 ),
-                // Row(
-                //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                //         children: const [
-                //           Expanded(
-                //               child: Padding(
-                //             padding: EdgeInsets.only(right: 18, left: 18),
-                //             child: Divider(
-                //               thickness: 2,
-                //               color: Colors.black,
-                //               height: 5,
-                //             ),
-                //           )),
-                //           Text(
-                //             'Or',
-                //             style: TextStyle(fontSize: 20),
-                //           ),
-                //           Expanded(
-                //               child: Padding(
-                //             padding: EdgeInsets.only(right: 22, left: 22),
-                //             child: Divider(
-                //               thickness: 2,
-                //               color: Colors.black,
-                //               height: 5,
-                //             ),
-                //           )),
-                //         ],
-                //       ),
                 Expanded(
                   child: Padding(
                     padding: const EdgeInsets.all(5.0),
