3c10babee705ff2f5e753d2a7f785dab3ce86ee9
iambinshad <binshadp999@gmail.com>
2023-03-23 15:13:51 +0530

    vendor endpoints add

--- lib/core/api/api_configration.dart
+++ lib/core/api/api_configration.dart
@@ -2,9 +2,18 @@ class ApiConfigration{
   static const kIpAddress = '10.4.3.119';
   static const kBaseUrl = "http://$kIpAddress:3500";
 
-  //userEndPoints
+  //----User Endpoints---
   static const login = '/login';
   static const otp = '/OTP';
   static const verifyOtp = '/verifyOTP';
+
+  //---Vendor Endpoints---
+
+  //after kBaseUrl 
+  static const vendor = '/vendor';
+
+  static const vendorLogin = '/login';
+  static const vendorOTP = '/vendorOTP';
+  static const verifyVendorOTP = '/verifyVendorOTP';
  
 }
