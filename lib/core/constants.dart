d8327dfbacb5ae08fb8d6f233055a39d653749d8
iambinshad <binshadp999@gmail.com>
2023-03-26 17:36:04 +0530

    set some constant variable for easy coding

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,11 +1,8 @@
-// import 'package:flutter/material.dart';
 
-// const kHeight10 = SizedBox(height: 10,);
-// const KHeight15 = SizedBox(height: 15,);
-// const kHeight20 = SizedBox(height: 20,);
-// const kHeight30 = SizedBox(height: 30,);
-// const kHeight40 = SizedBox(height: 40,);
-// const kWidth10 = SizedBox(width: 10,);
-// const kWidth20 = SizedBox(width: 20,);
-// const kWidth30 = SizedBox(width: 30,);
-// const kWidth40 = SizedBox(width: 40,);
+import 'package:flutter/material.dart';
+
+ var mainColor = const Color.fromARGB(255, 123, 230, 219);
+
+ var normalText = const TextStyle(fontSize: 19,fontWeight: FontWeight.bold);
+ var mediumText = const TextStyle(fontSize: 20,fontWeight: FontWeight.bold);
+  var headText = const TextStyle(fontSize: 24,fontWeight: FontWeight.bold,color:Colors.white);
